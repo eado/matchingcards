2ed9df01ba09e04e19d6afb782e22164db387242
Omar Elamri <ea_do@icloud.com>
2020-08-12 15:15:15 -0500

    Add initial files

---
+++ matchingcards/matchingcards/matchingcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  matchingcardsApp.swift
+//  matchingcards
+//
+//  Created by Omar Elamri on 8/12/20.
+//
+
+import SwiftUI
+
+@main
+struct matchingcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
